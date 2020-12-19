1f517dd196e2aedbdec33ffaede2ad94460f8fd6
Mohammadreza Taikandi <mrtaikandi@hotmail.com>
2020-12-19 08:08:19 +0000

    Add factory method.

---
+++ MapTo/Extensions/StringBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace MapTo.Extensions
+{
+    internal static class StringBuilderExtensions
+    {
+        public static StringBuilder PadLeft(this StringBuilder builder, int width)
+        {
+            for (var i = 0; i < width; i++)
+            {
+                builder.Append(" ");
+            }
+
+            return builder;
+        }
+
+        internal static StringBuilder AppendOpeningBracket(this StringBuilder builder, int indent = 0)
+        {
+            return builder.AppendLine().PadLeft(indent).AppendFormat("{{{0}", Environment.NewLine);
+        }
+
+        internal static StringBuilder AppendClosingBracket(this StringBuilder builder, int indent = 0, bool padNewLine = true)
+        {
+            if (padNewLine)
+            {
+                builder.AppendLine();
+            }
+
+            return builder.PadLeft(indent).Append("}");
+        }
+    }
+}
