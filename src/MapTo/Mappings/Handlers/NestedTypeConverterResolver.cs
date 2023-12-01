27d2ca88928e376759c2f24290b2c478d8dfa16f
Mohammadreza Taikandi <mrtaikandi@hotmail.com>
2023-12-01 10:57:56 +0000

    Allow configure enum mapping via MapFromAttribute directly from the enum declaration.

--- src/MapTo/Mappings/Handlers/NestedTypeConverterResolver.cs
+++ src/MapTo/Mappings/Handlers/NestedTypeConverterResolver.cs
@@ -3,33 +3,30 @@
 internal class NestedTypeConverterResolver : ITypeConverterResolver
 {
     /// <inheritdoc />
-    public virtual ResolverResult<TypeConverterMapping> Get(MappingContext context, IPropertySymbol property, SourceProperty sourceProperty)
+    public ResolverResult<TypeConverterMapping> Get(MappingContext context, IPropertySymbol property, SourceProperty sourceProperty)
     {
-        var mapFromAttribute = property.Type.GetAttribute(context.KnownTypes.MapFromAttributeTypeSymbol);
-        if (mapFromAttribute is null)
-        {
-            return ResolverResult.Undetermined<TypeConverterMapping>();
-        }
-
-        if (mapFromAttribute.ConstructorArguments.First().Value is not INamedTypeSymbol mappedSourcePropertyType)
-        {
-            return ResolverResult.Undetermined<TypeConverterMapping>();
-        }
-
         var methodPrefix = context.CodeGeneratorOptions.MapMethodPrefix;
-        return new TypeConverterMapping(
-            mappedSourcePropertyType.ToExtensionClassName(context.CodeGeneratorOptions),
-            mappedSourcePropertyType.IsPrimitiveType() ? $"{methodPrefix}{mappedSourcePropertyType.Name}" : $"{methodPrefix}{property.Type.Name}",
-            property.Type.ToTypeMapping(),
-            null,
-            true,
-            UsingDirectives: ImmutableArray.Create("global::System.Linq"),
-            ReferenceHandling: context.CodeGeneratorOptions.ReferenceHandling switch
-            {
-                ReferenceHandling.Disabled => false,
-                ReferenceHandling.Enabled => true,
-                ReferenceHandling.Auto when mappedSourcePropertyType.HasNonPrimitiveProperties() => true,
-                _ => false
-            });
+        var mapFromAttribute = property.Type.GetAttribute(context.KnownTypes.MapFromAttributeTypeSymbol);
+        var mappedSourcePropertyType = mapFromAttribute?.ConstructorArguments.First().Value as INamedTypeSymbol;
+
+        return mappedSourcePropertyType switch
+        {
+            null => ResolverResult.Undetermined<TypeConverterMapping>(),
+            { TypeKind: TypeKind.Enum } => ResolverResult.Undetermined<TypeConverterMapping>(), // Is handled by EnumTypeConverterResolver
+            _ => new TypeConverterMapping(
+                mappedSourcePropertyType.ToExtensionClassName(context.CodeGeneratorOptions),
+                mappedSourcePropertyType.IsPrimitiveType() ? $"{methodPrefix}{mappedSourcePropertyType.Name}" : $"{methodPrefix}{property.Type.Name}",
+                property.Type.ToTypeMapping(),
+                null,
+                true,
+                UsingDirectives: ImmutableArray.Create("global::System.Linq"),
+                ReferenceHandling: context.CodeGeneratorOptions.ReferenceHandling switch
+                {
+                    ReferenceHandling.Disabled => false,
+                    ReferenceHandling.Enabled => true,
+                    ReferenceHandling.Auto when mappedSourcePropertyType.HasNonPrimitiveProperties() => true,
+                    _ => false
+                })
+        };
     }
 }
